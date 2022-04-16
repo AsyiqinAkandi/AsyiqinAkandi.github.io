5fc649405bba8f2e1029c687776212e70e94ddab
AsyiqinAkandi <20FTT1461@student.pb.edu.bn>
2022-04-16 12:38:08 +0800

    change image

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
   <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
 
   <div id="app" class="shadow-md rounded-md grid grid-cols-1 gap-4 m-auto w-4/5 bg-gray-100 p-8">
-    <img src="./assets/pfp.jpg" alt="Profile Picture" class="shadow-md rounded-full w-20 h-20 place-content-center m-auto">
+    <img src="./assets/pfp.png" alt="Profile Picture" class="shadow-md rounded-full w-20 h-20 place-content-center m-auto">
     <h3 class="text-2xl underline">Khairunnisa' Asyiqin binti Akandi</h3>
     <div id="info">
       <p>School: School of Information and Technology</p>
